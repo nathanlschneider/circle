4455d38660c0cd4347bef6a0a785b3e629d58b4e
Nate Schneider <nschneider@mhgi.net>
2019-04-01 16:52:45 -0400

    added main files

---
+++ circle.js
@@ -0,0 +1,14 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    var root = document.body.style;
+    console.log(root);
+  
+    // Initialize variables
+    root.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  
+    // Scroll events
+    window.addEventListener('scroll',scroll,false);
+    function scroll() {
+      root.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+      console.log(window.pageYOffset / (document.body.offsetHeight - window.innerHeight))
+    }
+  });
